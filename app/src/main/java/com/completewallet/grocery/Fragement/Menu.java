55cfd963fb63aaeab65f4e1d552b360608b4ce53
Shaikh Aquib <shaikhaquib119@gmail.com>
2018-05-05 22:22:30 +0530

    first commite

--- app/src/main/java/com/completewallet/grocery/Fragement/Menu.java
+++ app/src/main/java/com/completewallet/grocery/Fragement/Menu.java
@@ -1,10 +1,30 @@
 package com.completewallet.grocery.Fragement;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.completewallet.grocery.R;
 
 /**
  * Created by Shaikh Aquib on 10-Apr-18.
  */
 
 public class Menu extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.usermenu, container, false);
+
+        return  rootview;
+    }
+
 }
+
+
